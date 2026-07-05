72546abdaf1630db8b1d4b08a00d5d4028cc6097
Sneha Mishra <146378201+Sneha-Mshr@users.noreply.github.com>
2026-07-06 00:12:37 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       int n=s.size();
+       int m=t.size();
+       if(n!=m) return false;
+       vector<int>ans(26,0);
+       for(int i=0;i<n;i++){
+         ans[s[i]-'a']++;
+         ans[t[i]-'a']--;
+       }
+
+       for(int i=0;i<26;i++){
+        if(ans[i]!=0) return false;
+       }
+       return true;
+
+    }
+};
